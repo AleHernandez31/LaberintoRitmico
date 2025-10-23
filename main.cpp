5ffe1f23ca8ed3e1cbb6dc032c2a9797f1f5ed58
Ricardo Gieco <162388768+RicardoGieco@users.noreply.github.com>
2025-10-23 17:37:44 -0300

    cambie el main para eliminar la cortina negra

--- main.cpp
+++ main.cpp
@@ -124,27 +124,35 @@ int main() {
                 menu.dibujar(window);
                 break;
 
-            case JUGANDO:
-                // Agregar Imagenes del Gameplay
-                viewGame.setCenter(player.getPosMundo()); // Centro la vista sobre el jugador.
-                window.setView(viewGame);
-                //map.draw2x2Framed(window, player.getWorldPos(), player.getPosGrilla().x, player.getPosGrilla().y); // Dibujo mapa centrado sobre la posición del jugador.
-                map.draw2x2Framed(window, player.getPosGrilla());
-                window.draw(player); // Dibujo Player
-                break;
+            case JUGANDO: //lucas agregué un prefetch para corregir la cortina negra del juego, agregué codigo pero no cambie lo tuyo.
+                          // el problema era que al setear al view de la ventana pinta el proxima mapa al final, ya que sigue al jugador. seria un (actualizo camara, fijo view, pinto el mapa y pinto al jugador. tonces aparece esa cortina negra hasta que el proximo chunk 2x2 se dibuje.
 
-            case CONTROLES:
-                window.setView(viewUI);
-                // Agregar contenido de controles
-                break;
+    // Agregar Imagenes del Gameplay
+    viewGame.setCenter(player.getPosMundo()); // Centro la vista sobre el jugador. (mejora el seguimiento del dash)
+    window.setView(viewGame);
 
-            case PUNTAJES:
-                window.setView(viewUI);
-                // Agregar contenido de puntajes
-                break;
+    //agregue un "prefetch" que encontre de un chabon que lo que hace es dibujar por adelantado los chunks vecinos, asi cuando la camara sigue al pj, ya esta esa parte cargada en el borde y no se nota la cortina.
+    const sf::Vector2i grid = player.getPosGrilla(); //tome el centro del mundo segun el chunk actual donde esta el pj
+    map.draw2x2Framed(window, grid);
 
-            case SALIR:
-                break;
+    //Con esto logro que se dibujen los chunks vecinos a medida que la camara/player se aleja de la antigua interseccion/centro.
+    const sf::Vector2f world   = player.getPosMundo(); //marco la posicion del pj.
+    const sf::Vector2f centerG = { grid.x * Map::CELL, grid.y * Map::CELL }; //multiplico por map cell para conseguir la posicion en el mapa donde va caer la proxima interseccion.
+    const sf::Vector2f delta   = world - centerG; //calculo cuanto se dezplazo el pj desde el chunk anterior. asi puedo marcar un umbral para generar los otros chunks antes de que se genere la cortina.
+
+    // el umbral es un valor "limite" que voy a usar como señal para decidir cuando generar los chunks vecinos.
+    // imaginenlo como un circulo que cuando estas adentro es TH negativo, pero cuando salis es TH positivo y ahi meto el prefetch para dibujar por adelantado el chunk que sigue. (si,estoy enfermo).
+    const float TH = Map::CELL * 0.25f; // lo defini como el 25% del tamaño de una manzana. tonces si se aleja mas del 25% del centro, asumimos la direccion a la que va y generamos ese chunk vecino.
+    int sx = (delta.x >  TH) ? +1 : (delta.x < -TH ? -1 : 0); //aca segun sx = (+1 este); (0 oeste).
+    int sy = (delta.y >  TH) ? +1 : (delta.y < -TH ? -1 : 0);// segun sy = (+1 sur); (0 norte).
+
+    if (sx != 0) map.draw2x2Framed(window, grid + sf::Vector2i(sx, 0)); //si va hacia el este u oeste, se dibuja el vecino lateral.
+    if (sy != 0) map.draw2x2Framed(window, grid + sf::Vector2i(0, sy));// lo mismo para norte y sur.
+    if (sx != 0 && sy != 0) map.draw2x2Framed(window, grid + sf::Vector2i(sx, sy));//esto es para cuando se mueva en diagonal, por si llegamos a agregar que al inicio sale de la plaza y el primer dash es diagonal para llegar a una esquina, lo dejo armado para tenerlo  en cuenta.
+
+    window.draw(player); // Dibujo Player
+    break;
+
         }
 
         // Display
